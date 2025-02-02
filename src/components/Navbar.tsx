03005f23d01b5b3ee33879a3e4941c75647f3d4b
zzLeon <159605217+zzLeonn@users.noreply.github.com>
2025-02-02 16:14:04 +0630

    change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <Link to="/" className="flex items-center">
-            <img src="public/logo.png" alt="Repunzel" className="h-12" />
+            <img src="/logo.png" alt="Repunzel" className="h-12" />
           </Link>
 
           {/* Desktop Navigation */}
