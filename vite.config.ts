788cd903312eba102d1348a149b58f2d11640e0b
zzLeon <159605217+zzLeonn@users.noreply.github.com>
2025-02-02 16:24:53 +0630

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // React plugin for Vite
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(), // Enable Tailwind CSS
+        autoprefixer(), // Enable autoprefixer
+      ],
+    },
+  },
+});
